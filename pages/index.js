e219c54a50e1c95752c456e3ac880e2c30f39f1a
devhasibulislam <devhasibulislam@gmail.com>
2023-11-02 15:11:36 +0600

    init basic meta tags

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,24 @@
  */
 
 import Gallery from "@/components/Gallery";
+import Head from "next/head";
 import React from "react";
 
 const Index = () => {
-  return <Gallery />;
+  return (
+    <>
+      <Head>
+        <title>Simple Image Gallery</title>
+        <meta
+          name="description"
+          content="Create a responsive image gallery using React JS with the following features: reordering, deleting multiple images, and setting a feature image. The gallery should be visually appealing and provide a seamless user experience."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Gallery />
+    </>
+  );
 };
 
 export default Index;
