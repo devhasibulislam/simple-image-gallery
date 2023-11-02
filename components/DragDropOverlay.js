ff7ca7638358f19a9d00b2df5f8aa00fb7244683
devhasibulislam <devhasibulislam@gmail.com>
2023-11-02 15:07:04 +0600

    integrate z-50 index so that always on top

--- components/DragDropOverlay.js
+++ components/DragDropOverlay.js
@@ -19,7 +19,7 @@ const DragDropOverlay = ({ dragging, draggedIndex, image }) => {
   return (
     dragging &&
     Number(draggedIndex) === Number(image.id) && (
-      <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-white border-2 border-dashed rounded-lg">
+      <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-white border-2 border-dashed rounded-lg z-50">
         Drop Here
       </div>
     )
