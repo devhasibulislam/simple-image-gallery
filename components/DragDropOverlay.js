b2b4f45c1a34576f87548779e8698dd19a20f474
devhasibulislam <devhasibulislam@gmail.com>
2023-11-02 14:50:16 +0600

    drag and drop component initialize

---
+++ components/DragDropOverlay.js
@@ -0,0 +1,29 @@
+/**
+ * Title: Write a program using JavaScript on DragDropOverlay
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 02, November 2023
+ */
+
+import React from "react";
+
+const DragDropOverlay = ({ dragging, draggedIndex, image }) => {
+  return (
+    dragging &&
+    Number(draggedIndex) === Number(image.id) && (
+      <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-white border-2 border-dashed rounded-lg">
+        Drop Here
+      </div>
+    )
+  );
+};
+
+export default DragDropOverlay;
