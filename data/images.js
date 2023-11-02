8418d3327d541dbc225df23c1a04a182e444b0c3
devhasibulislam <devhasibulislam@gmail.com>
2023-11-02 10:04:04 +0600

    create a mock data from given images

---
+++ data/images.js
@@ -0,0 +1,30 @@
+/**
+ * Title: Write a program using JavaScript on Images
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 01, November 2023
+ */
+
+const images = [
+  { id: 1, thumbnail: "/assets/image-1.webp" },
+  { id: 2, thumbnail: "/assets/image-2.webp" },
+  { id: 3, thumbnail: "/assets/image-3.webp" },
+  { id: 4, thumbnail: "/assets/image-4.webp" },
+  { id: 5, thumbnail: "/assets/image-5.webp" },
+  { id: 6, thumbnail: "/assets/image-6.webp" },
+  { id: 7, thumbnail: "/assets/image-7.webp" },
+  { id: 8, thumbnail: "/assets/image-8.webp" },
+  { id: 9, thumbnail: "/assets/image-9.webp" },
+  { id: 10, thumbnail: "/assets/image-10.jpeg" },
+  { id: 11, thumbnail: "/assets/image-11.jpeg" },
+];
+
+export default images;
